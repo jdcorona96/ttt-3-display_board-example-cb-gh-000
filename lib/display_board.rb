22e2453795bf7d9c916038ce93b0d530bdc78a62
Joseph Corona <josed.hc96@gmail.com>
2018-11-03 13:55:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,8 @@
 def display_board
 
   puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-
-  board
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
